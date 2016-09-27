eeddfccd3cacdcb7f1f32be838846f0675bd5f13
gnmmarechal <gnmpolicemata91@gmail.com>
2016-09-27 16:52:58 +0100

    Ex. 1.9

---
+++ perarea.java
@@ -0,0 +1,54 @@
+/*
+ * perarea.java
+ * 
+ * Copyright 2016 Mário Alexandre Lopes Liberato <mliberato@ua.pt>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+//Objectivo: A partir de 2 valores inseridos, apresentar o perímetro e a área de um rectângulo definido pelos mesmos.
+
+
+import java.util.*;
+
+
+public class perarea 
+{
+	
+	public static void main (String args[]) 
+	{
+		Scanner sca = new Scanner(System.in);
+		System.out.print("Insira o comprimento:");
+		double a = sca.nextDouble();
+		System.out.print("Insira a largura:");
+		double b = sca.nextDouble();
+		
+		// Calcular c*l e 2(c+l)
+		
+		double area = a*b;
+		double peri = 2*(a+b);
+		
+		System.out.printf("Área: %f u.a.\nComprimento: %f u.c.", area, peri);
+		
+		
+		
+		
+	}
+}
+
